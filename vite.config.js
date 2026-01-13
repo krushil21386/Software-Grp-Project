df8f243f0886e950fd8d8c92264934c0a8f8619b
Krushil-2126 <24dit043@charusat.edu.in>
2026-01-13 15:51:17 +0530

    Fix GitHub Pages base path and assets

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Software-group-project/',
+  base: '/Software-Grp-Project/',
   plugins: [react()],
 })
